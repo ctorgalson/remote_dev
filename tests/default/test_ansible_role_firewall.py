9b545cdd52517e65025bfeee3b62b3397377b2b9
Christopher Torgalson <manager@bedlamhotel.com>
2020-05-16 16:41:56 +0200

    Further changes to enable public webserver for dev machine

--- tests/default/test_ansible_role_firewall.py
+++ tests/default/test_ansible_role_firewall.py
@@ -12,15 +12,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 """ ansible-role-firewall (geerlingguy.firewall) tests. """
 
 
-@pytest.mark.parametrize('service_or_port', [
-    22,
-    80,
-    123,
-    443,
-    60033,
+@pytest.mark.parametrize('port,protocol', [
+    (22, 'tcp'),
+    (80, 'tcp'),
+    (123, 'udp'),
+    (443, 'tcp'),
+    (60033, 'tcp'),
 ])
-def test_open_ports(host, service_or_port):
+def test_open_ports(host, port, protocol):
     r = host.iptables.rules()
-    l = '-A INPUT -p tcp -m tcp --dport {} -j ACCEPT'.format(service_or_port)
+
+    if protocol == 'tcp':
+        l = '-A INPUT -p tcp -m tcp --dport {} -j ACCEPT'.format(port)
+    else:
+        l = '-A INPUT -p udp -m udp --sport {} -j ACCEPT'.format(port)
 
     assert l in r
