606c02dcb6468dcde3291ea0ad21f20f6c9d4b5b
Christopher Torgalson <manager@bedlamhotel.com>
2020-05-16 22:52:21 +0200

    Updates clamav config, docksal test

--- tests/default/test_ansible_role_docksal.py
+++ tests/default/test_ansible_role_docksal.py
@@ -9,7 +9,14 @@ import pytest
 
 
 @pytest.mark.parametrize('contents', [
+    'APACHE_BASIC_AUTH_USER="username"',
+    'APACHE_BASIC_AUTH_PASS="password"',
+    'DOCKSAL_DNS_DOMAIN="instance.example"',
     'DOCKSAL_VHOST_PROXY_IP="0.0.0.0"',
+    'DOCKSAL_VHOST_PROXY_PORT_HTTP="80"',
+    'DOCKSAL_VHOST_PROXY_PORT_HTTPS="443"',
+    'NGINX_BASIC_AUTH_USER="username"',
+    'NGINX_BASIC_AUTH_PASS="password"',
     'SECRET_PLATFORMSH_CLI_TOKEN="109876543210"',
 ])
 def test_docksal_config(host, contents):
